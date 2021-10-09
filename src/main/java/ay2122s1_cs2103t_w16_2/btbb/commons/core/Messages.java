ec1705af7e1bd68faacff1a7f79060dc41115b3f
Imran <imranr2@msn.com>
2021-10-09 22:23:15 +0800

    Add message for finding orders

--- src/main/java/ay2122s1_cs2103t_w16_2/btbb/commons/core/Messages.java
+++ src/main/java/ay2122s1_cs2103t_w16_2/btbb/commons/core/Messages.java
@@ -8,4 +8,5 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "The client index provided is invalid";
     public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d clients listed!";
+    public static final String MESSAGE_ORDERS_LISTED_OVERVIEW = "%1$d orders listed!";
 }
