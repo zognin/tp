f8741a4cfc64a6f924208b4c0e1946ea86747613
Imran <imranr2@msn.com>
2021-10-09 22:20:39 +0800

    Add method to get typical orders

--- src/test/java/ay2122s1_cs2103t_w16_2/btbb/testutil/TypicalOrders.java
+++ src/test/java/ay2122s1_cs2103t_w16_2/btbb/testutil/TypicalOrders.java
@@ -10,6 +10,11 @@ import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalClients.ELLE;
 import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalClients.FIONA;
 import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalClients.GEORGE;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ay2122s1_cs2103t_w16_2.btbb.model.AddressBook;
 import ay2122s1_cs2103t_w16_2.btbb.model.order.Order;
 
 /**
@@ -45,4 +50,19 @@ public class TypicalOrders {
     public static final Order ORDER_FOR_BOB =
             new OrderBuilder().withClientName(BOB.getName()).withClientPhone(BOB.getPhone())
                     .withClientAddress(BOB.getAddress()).build();
+    /**
+     * Returns an {@code AddressBook} with all the typical orders.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Order order : getTypicalOrders()) {
+            ab.addOrder(order);
+        }
+        return ab;
+    }
+
+    public static List<Order> getTypicalOrders() {
+        return new ArrayList<>(Arrays.asList(ORDER_FOR_ALICE, ORDER_FOR_BENSON, ORDER_FOR_CARL, ORDER_FOR_DANIEL,
+                ORDER_FOR_ELLE, ORDER_FOR_FIONA, ORDER_FOR_GEORGE));
+    }
 }
