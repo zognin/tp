b8f270ed2eb492e14b684d8820a944d8b9243d5b
Imran <imranr2@msn.com>
2021-10-19 15:30:59 +0800

    Add test case for getTopTenClients

--- src/test/java/ay2122s1_cs2103t_w16_2/btbb/model/order/UniqueOrderListTest.java
+++ src/test/java/ay2122s1_cs2103t_w16_2/btbb/model/order/UniqueOrderListTest.java
@@ -4,19 +4,40 @@ import static ay2122s1_cs2103t_w16_2.btbb.testutil.Assert.assertThrows;
 import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_ALICE;
 import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_AMY;
 import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_BENSON;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_BOB;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_CARL;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_DANIEL;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_ELLE;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_FIONA;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_GEORGE;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_HOON;
+import static ay2122s1_cs2103t_w16_2.btbb.testutil.TypicalOrders.ORDER_FOR_IDA;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 
 import ay2122s1_cs2103t_w16_2.btbb.exception.NotFoundException;
+import ay2122s1_cs2103t_w16_2.btbb.model.client.Phone;
+import ay2122s1_cs2103t_w16_2.btbb.model.shared.GenericString;
+import ay2122s1_cs2103t_w16_2.btbb.testutil.OrderBuilder;
+import javafx.util.Pair;
 
 class UniqueOrderListTest {
     private final UniqueOrderList uniqueOrderList = new UniqueOrderList();
+
+    @Test
+    public void add_nullOrder_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueOrderList.add(null));
+    }
 
     @Test
     public void contains_nullOrder_throwsNullPointerException() {
@@ -35,8 +56,26 @@ class UniqueOrderListTest {
     }
 
     @Test
-    public void add_nullOrder_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> uniqueOrderList.add(null));
+    public void getTopTenClients_listContainsOrdersForElevenClients_returnsTopTenClients() {
+        List<Order> orderList = new ArrayList<>(List.of(ORDER_FOR_ALICE, ORDER_FOR_AMY, ORDER_FOR_BOB, ORDER_FOR_BENSON,
+                ORDER_FOR_CARL, ORDER_FOR_DANIEL, ORDER_FOR_ELLE, ORDER_FOR_FIONA, ORDER_FOR_GEORGE, ORDER_FOR_HOON,
+                ORDER_FOR_IDA));
+        IntStream.range(1, orderList.size() + 1).forEach(i -> {
+            IntStream.range(0, i).forEach(j -> uniqueOrderList.add(new OrderBuilder(orderList.get(i - 1))
+                    .withPrice(new Price(Integer.toString(i)))
+                    .build()));
+        });
+        List<Map.Entry<Pair<GenericString, Phone>, Integer>> topTenClients = uniqueOrderList.getTopTenClients();
+
+        orderList.remove(ORDER_FOR_ALICE);
+        List<Map.Entry<Pair<GenericString, Phone>, Integer>> expectedTopTenClients = new ArrayList<>();
+        IntStream.range(0, 10).forEach(i -> {
+            Order currOrder = orderList.get(i);
+            expectedTopTenClients.add(new AbstractMap.SimpleEntry<>(new Pair<>(currOrder.getClientName(),
+                    currOrder.getClientPhone()), i + 2));
+        });
+        Collections.reverse(expectedTopTenClients);
+        assertEquals(expectedTopTenClients, topTenClients);
     }
 
     @Test
