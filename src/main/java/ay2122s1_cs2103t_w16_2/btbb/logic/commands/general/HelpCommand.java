3bf85c9486f366f0b95101827414d14c7ed17c59
Eunice <eunicesimsingyee@gmail.com>
2021-10-19 23:41:40 +0800

    Add logging for general commands

--- src/main/java/ay2122s1_cs2103t_w16_2/btbb/logic/commands/general/HelpCommand.java
+++ src/main/java/ay2122s1_cs2103t_w16_2/btbb/logic/commands/general/HelpCommand.java
@@ -1,5 +1,9 @@
 package ay2122s1_cs2103t_w16_2.btbb.logic.commands.general;
 
+import java.util.logging.Logger;
+
+import ay2122s1_cs2103t_w16_2.btbb.commons.core.LogsCenter;
+import ay2122s1_cs2103t_w16_2.btbb.commons.util.JsonUtil;
 import ay2122s1_cs2103t_w16_2.btbb.logic.commands.Command;
 import ay2122s1_cs2103t_w16_2.btbb.logic.commands.CommandResult;
 import ay2122s1_cs2103t_w16_2.btbb.model.Model;
@@ -15,8 +19,12 @@ public class HelpCommand extends Command {
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
 
+    private static final Logger logger = LogsCenter.getLogger(JsonUtil.class);
+
     @Override
     public CommandResult execute(Model model) {
+        logger.info("Executing " + HelpCommand.class.getSimpleName());
+
         return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
     }
 }
